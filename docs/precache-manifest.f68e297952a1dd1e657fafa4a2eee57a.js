5207cc7e90175ed54a13d1b5a505d37dae89fcd1
lararb <lararb91@gmail.com>
2019-05-25 23:02:35 +0200

    Create docs folder

---
+++ docs/precache-manifest.f68e297952a1dd1e657fafa4a2eee57a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d21276457948e442bccbc739e7e243e9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7e093c053a243cd137ef",
+    "url": "./static/css/main.20c96358.chunk.css"
+  },
+  {
+    "revision": "db3927a79f0d16c235fc",
+    "url": "./static/js/2.13ef48b5.chunk.js"
+  },
+  {
+    "revision": "7e093c053a243cd137ef",
+    "url": "./static/js/main.afc32ee0.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
